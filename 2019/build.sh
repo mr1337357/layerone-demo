aa98149f97e57640d3df17b7f465393fa7901b54
misspapaya <missp@pa.ya>
2020-02-18 23:15:30 -0800

    updated build/flash script and readme

--- 2019/build.sh
+++ 2019/build.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
-#CYGWIN
-CC="/cygdrive/c/Program Files (x86)/Microchip/xc16/v1.36/bin/xc16-gcc.exe"
+load() {
+   mono ds30LoaderConsole.exe -f=out/demo.hex -d=pic24fj256da206 -k=/dev/ttyACM0 -r=115200 --writef -o
 
-BIN2HEX="/cygdrive/c/Program Files (x86)/Microchip/xc16/v1.36/bin/xc16-bin2hex.exe"
+}
+
+host=$(uname -s)
+case $host in
+   Linux*)
+      CC="/opt/microchip/xc16/v1.50/bin/xc16-gcc"
+      BIN2HEX="/opt/microchip/xc16/v1.50/bin/xc16-bin2hex"
+      LOADER="mono ds30LoaderConsole.exe"
+      ;;
+   CYGWIN*)
+      CC="/cygdrive/c/Program Files (x86)/Microchip/xc16/v1.36/bin/xc16-gcc.exe"
+      BIN2HEX="/cygdrive/c/Program Files (x86)/Microchip/xc16/v1.36/bin/xc16-bin2hex.exe"
+      ;;
+   *)
+      echo Unknown platform $host
+      exit 1
+esac
 
 CFLAGS="-mcpu=24FJ256DA206 -mlarge-arrays -mlarge-code -Wall"
 SRCDIR=src
@@ -30,3 +46,5 @@ done
 "${CC}" ${OBJS} -o ${OUTDIR}/demo.elf -mcpu=24FJ256DA206 -omf=elf -mlarge-arrays -Wl,--local-stack,--defsym=__MPLAB_BUILD=1,,--script=p24FJ256DA206.gld,--stack=16,--check-sections,--data-init,--pack-data,--handles,--isr,--no-gc-sections,--fill-upper=0,--stackguard=16,--no-force-link,--smart-io,-Map=production.map,--report-mem > meminfo.log
 
 "${BIN2HEX}" ${OUTDIR}/demo.elf -a  -omf=elf
+ 
+load
---
+++ README.md
@@ -0,0 +1,9 @@
+#LAYER ONE DEMOS FROM PREVIOUS YEARS
+
+##2015
+Just a 4 part harmony of still alive from portal. Later added graphics after demo party was over.
+
+##2019
+Rewrote synthesizer to use samples instead of square waves. Forgot to update song. Added Scrolling rainbow background and spinning conference logo.
+Actual presentation at the 2019 party
+https://www.youtube.com/watch?v=Sf5sX0QKMOM&t=1002s
